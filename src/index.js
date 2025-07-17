339e978f074706a3cf2cfa2f2da2ae0ec48c33fd
mlpzz47 <mlpezmn@gmail.com>
2025-07-17 18:56:11 +0200

    html and css basic structure

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+const inboxBtn = document.getElementById('inbox-btn');
+const todayBtn = document.getElementById('today-btn');
+const thisWeekBtn = document.getElementById('this-week-btn');
+const addProjectBtn = document.getElementById('add-project-btn');
+const mainTitle = document.querySelector('.main__title');
+// const inboxBtn = document.getElementById('inbox-btn');
+// const inboxBtn = document.getElementById('inbox-btn');
+
+inboxBtn.addEventListener('click',);
